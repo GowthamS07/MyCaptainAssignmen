7cf18a504bc225cf1c6e92168db1dad1044ef7a9
Gowtham S <32368731+GowthamS07@users.noreply.github.com>
2022-07-11 21:49:04 +0530

    Add files via upload

---
+++ july.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 11 21:39:12 2022
+
+@author: Gowtham S
+"""
+"""
+1. Do not call me sir
+2. 10% reply
+3. Do not be rude
+"""
+
+print("Hello World")
+
